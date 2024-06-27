ad0300cb7575c6a3824b76646ca901627d475a6d
shin-suhyeon <165615546+shin-suhyeon@users.noreply.github.com>
2024-06-27 12:02:32 +0900

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -1,24 +1,5 @@
 const visual = new Swiper('.visual',{
-    // autoplay:{delay:5000},
+    autoplay:{delay:3000},
     /* effect:'fade', */
     effect:'horizontal',
-    })
-const event = new Swiper('.event',{
-    // autoplay:{delay:3000},
-    direction:'horizontal',
-    slidesPerView: 3,
-    spaceBetween: 0,
-    loop:'true',
-})
-const yunjin = new Swiper('.yunjin',{
-    // autoplay:{delay:3000},
-    direction:'horizontal',
-    loop:'true',
-})
-const Routine = new Swiper('.Routine',{
-    // autoplay:{delay:3000},
-    direction:'horizontal',
-    slidesPerView: 3,
-    spaceBetween: 0,
-    loop:'true',
 })
