e3337bf9120fee37b75c4aa110c833b584ac888f
shin-suhyeon <165615546+shin-suhyeon@users.noreply.github.com>
2024-06-27 15:33:42 +0900

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -1,5 +1,8 @@
 const visual = new Swiper('.visual',{
     autoplay:{delay:3000},
-    /* effect:'fade', */
+    effect:'fade',
     effect:'horizontal',
+    pagination: {
+        el: '.bg1 .swiper-pagination',
+    },
 })
